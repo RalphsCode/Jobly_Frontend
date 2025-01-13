a8aa67cd46b4f76239240d2f1f5c66c12cd5fb01
RalphsCode <RalphsCode@gmail.com>
2025-01-12 23:09:10 -0800

    Added code to eliminate double slashes in URL concatenation.

--- src/api.js
+++ src/api.js
@@ -25,7 +25,9 @@ class JoblyApi {
 
     //there are multiple ways to pass an authorization token, this is how you pass it in the header.
     //this has been provided to show you another way to pass the token. you are only expected to read this code for this project.
-    const url = `${BASE_URL}/${endpoint}`;
+    
+      // Avoid double slashes in URL
+    const url = `${BASE_URL.replace(/\/$/, "")}/${endpoint}`;
     const headers = { Authorization: `Bearer ${JoblyApi.token}` };
     const params = (method === "get")
         ? data
